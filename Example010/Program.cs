7724bc8be9ee5db067510e34ba7462a66f9086e4
Berus Frager <gitmailru@yandex.ru>
2022-08-09 10:54:16 +0300

    Коммит файлов папки проекта Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+Console.WriteLine("Введите трехзначное целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99)
+{
+    if (number < 1000)
+    {
+        int twoDigits = number / 10;
+        int secondDigit = twoDigits % 10;
+        Console.WriteLine("Вторая цифра числа: " + secondDigit);
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не является трехзначным.");
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число не является трехзначным.");
+}
