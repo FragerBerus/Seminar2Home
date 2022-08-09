6b23cdc269ae479eace68734063467b797d21de2
Berus Frager <gitmailru@yandex.ru>
2022-08-09 12:06:32 +0300

    Коммит файлов папки проекта Example015

---
+++ Example015/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int numberWeek = Convert.ToInt32(Console.ReadLine());
+if (numberWeek > 0 && numberWeek < 8)
+{
+    if (numberWeek < 6)
+    {
+        Console.WriteLine(numberWeek);
+        Console.WriteLine("Указанный день не является выходным.");
+    }
+    else
+    {
+        Console.WriteLine(numberWeek);
+        Console.WriteLine("Указанный день является выходным.");
+    }
+}
+else
+{
+    Console.WriteLine("Неверный порядковый номер дня. В неделе всего 7 дней !!!");
+}
